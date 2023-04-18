a6e22b435ef8a37add8d617f3d2acf20c9423d81
Jihwan Kim <jihwan.kim@purple.io>
2023-04-18 17:53:59 +0900

    eslint 추가

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-const axios = require('axios');
-const body = { test: 'test' };
+const axios = require("axios");
+const body = { test: "test" };
 axios
-  .post('https://wms-api-beta.sta.kolonfnc.com/api/account', body)
+  .post("https://wms-api-beta.sta.kolonfnc.com/api/account", body)
   .then((res) => {
     return res.json();
   })
   .then((res) => {
     console.log(res);
-    console.log('성공');
+    console.log("성공");
   })
   .catch((err) => {
-    console.error('에러발생');
+    console.error("에러발생");
     console.error(err);
   });
