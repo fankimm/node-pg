ede7b764c19926241b9b970d6ce0f413a76999f4
Jihwan Kim <jihwan.kim@purple.io>
2023-04-18 15:18:32 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const body = { test: 'test' };
+axios
+  .post('https://wms-api-beta.sta.kolonfnc.com/api/account', body)
+  .then((res) => {
+    return res.json();
+  })
+  .then((res) => {
+    console.log(res);
+    console.log('성공');
+  })
+  .catch((err) => {
+    console.error('에러발생');
+    console.error(err);
+  });
